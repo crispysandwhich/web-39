a33733cb582cb8e3266cb0d1fcf5ad9ceb5df9de
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-03-11 13:09:52 -0500

    wip

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ const server = express();
 
 server.use(express.json())
 
-if(process.env.NODE_ENV !== 'production'){
+if(process.env.NODE_ENV !== 'development'){
   const cors = require('cors')
   server.use(cors())
 }
 
+
+server.use('*', (res,req) => {
+  res.send('<h1>hi world</h1>')
+})
+
+
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+  console.log(`listing on ${PORT}`)
+})
