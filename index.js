1d1ef9c32ab973ff49a6e697513e66b166a596ca
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-03-11 13:26:50 -0500

    wip

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 
 require('dotenv').config()
-
+const path = require('path')
 const express = require('express')
 
 const server = express();
 
 server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
+
+console.log(process.env.NODE_ENV)
 
 if(process.env.NODE_ENV === 'development'){
   const cors = require('cors')
   server.use(cors())
-  console.log(process.env.NODE_ENV)
 }
+// our api comes earlier in pipline
+server.get('/api/hello', (req,res) => {
+  res.json({message: 'hello'})
+} )
 
-
-server.use('*', (res,req) => {
-  res.send('<h1>hi world</h1>')
+// catch all that sends back inde.html
+server.use('*', (req,res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 
@@ -23,6 +29,5 @@ server.use('*', (res,req) => {
 const PORT = process.env.PORT || 4000
 
 server.listen(PORT, () => {
-  
   console.log(`listing on ${PORT}`)
 })
